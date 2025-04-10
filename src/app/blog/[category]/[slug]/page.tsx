ae88fe8e00ad1b63d4ba69c12b0dc687f608842e
john-e-moore <jmoore87jr@gmail.com>
2025-04-10 14:02:57 -0400

    made all blog post images responsive

--- src/app/blog/[category]/[slug]/page.tsx
+++ src/app/blog/[category]/[slug]/page.tsx
@@ -8,9 +8,9 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { category, slug } = await params;
   const { meta, content } = await getPostBySlug(category, slug);
   return (
-    <article style={{ lineHeight: 1.5 }}>
+    <article>
       <h1>{meta.title}</h1>
-      <h3>{meta.date}</h3>
+      <p>{meta.date}</p>
       {meta.image && (
         <img
           src={`/blog_posts/${meta.category}/${meta.image}`}
@@ -18,7 +18,8 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
           className="post-image"
         />
       )}
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      {/* Wrap the markdown-generated HTML */}
+      <div className="post-content" dangerouslySetInnerHTML={{ __html: content }} />
     </article>
   );
 }
